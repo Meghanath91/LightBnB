67df85aebbd7356d39a68169900b98ca8ce0a5d1
Meghanath91 <meghanath.balaji@gmail.com>
2020-02-05 22:20:30 +0000

    fake data created

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO users(name,email,password)
+ values('arathy','ara@gml.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+ ('raj','ar@gml.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+ ('araty','arat@gml.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+ ('bob','arat@gml.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('kim','arat@gml.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+insert into reservations(start_date,end_date)
+Values('2018-02-03','2018-02-04'),
+('2018-02-05','2018-02-06'),
+('2018-02-07','2018-02-07');
+
+
+
